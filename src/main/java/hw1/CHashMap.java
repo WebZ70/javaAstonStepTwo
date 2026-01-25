6ca7b70f8dc337ce1774ada516c3c759f6742f12
WebZ70 <webz70@mail.ru>
2026-01-25 17:45:32 +0600

    add feature/hw1

---
+++ src/main/java/hw1/CHashMap.java
@@ -0,0 +1,108 @@
+package hw1;
+
+import java.util.Objects;
+
+public class CHashMap<K, V> {
+    private static final int DEFAULT_CAPACITY = 16;
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    private Item<K,V>[] buckets;
+    private int size;
+    public int lenght;
+
+    private static class Item<K,V> {
+        K key;
+        V value;
+        Item<K,V> next;
+
+        public Item(K key, V value, Item<K,V> next) {
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public CHashMap() {
+        buckets = new Item[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    private int indexSearch(K key) {
+        return Math.abs(Objects.hashCode(key)) % buckets.length;
+    }
+
+    public V put(K key, V value) {
+        int index = indexSearch(key);
+        Item<K,V> current = buckets[index];
+
+        while (current != null) {
+            if (current.key.hashCode() == key.hashCode()) {
+                if (current.key.equals(key)) {
+                    V oldValue = current.value;
+                    current.value = value;
+                    return oldValue;
+                }
+            }
+            current = current.next;
+        }
+        buckets[index] = new Item<>(key, value, buckets[index]);
+        if ((float) ++size / buckets.length > DEFAULT_LOAD_FACTOR) {resize();}
+        return null;
+    }
+
+    public V get(K key) {
+        int index = indexSearch(key);
+        Item<K,V> current = buckets[index];
+
+        while (current != null) {
+            if (current.key.hashCode() == key.hashCode()) {
+                if (current.key.equals(key)) {
+                    return current.value;
+                }
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int index = indexSearch(key);
+        Item<K,V> current = buckets[index];
+        Item<K,V> prev = null;
+
+        while (current != null) {
+            if (current.key.hashCode() == key.hashCode()) {
+                if (current.key.equals(key)) {
+                    if (prev == null) {
+                        buckets[index] = current.next;
+                    }else {
+                        prev.next = current.next;
+                    }
+                    size--;
+                    return current.value;
+                }
+            }
+            prev = current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    private void resize() {
+        Item<K, V>[] newBuckets = new Item[buckets.length * 2];
+
+        for (int i = 0; i < buckets.length; i++) {
+            Item<K, V> current = buckets[i];
+            while (current != null) {
+                Item<K, V> next = current.next;
+
+                int hash = Objects.hashCode(current.key); // Безопасно для null
+                int index = Math.abs(hash) % newBuckets.length;
+
+                current.next = newBuckets[index];
+                newBuckets[index] = current;
+                current = next;
+            }
+        }
+        buckets = newBuckets;
+    }
+}
---
+++ src/test/java/TestHW1.java
@@ -0,0 +1,59 @@
+import hw1.CHashMap;
+import org.junit.jupiter.api.Test;
+
+public class TestHW1 {
+    @Test
+    void putMap() {
+        CHashMap<Integer, String> cmap = new CHashMap<>();
+        cmap.put(0, "Zero");
+        cmap.put(1, "One");
+        cmap.put(2, "Two");
+        cmap.put(3, "Three");
+        cmap.put(4, "Four");
+        cmap.put(5, "Five");
+        cmap.put(6, "Six");
+        cmap.put(7, "Seven");
+        cmap.put(8, "Eight");
+        cmap.put(9, "Nine");
+        cmap.put(10, "Ten");
+        cmap.put(11, "Eleven");
+        cmap.put(12, "Twelve");
+        cmap.put(13, "Thirteen");
+        cmap.put(14, "Fourteen");
+
+        for (int i = 0; i < 15; i++) {
+            System.out.printf("%s : %s \n", i, cmap.get(i));
+        }
+    }
+
+    @Test
+    void getMap() {
+        CHashMap<Integer, Integer> cmap = new CHashMap<>();
+        for (int i = 0; i < 15; i++) {
+            cmap.put(i, i);
+        }
+
+        for (int i = 0; i < 15; i++) {
+            System.out.printf("%s : %s \n", i, cmap.get(i));
+        }
+
+    }
+
+    @Test
+    void removeMap() {
+        CHashMap<Integer, Integer> cmap = new CHashMap<>();
+        for (int i = 0; i < 15; i++) {
+            cmap.put(i, i);
+        }
+
+        cmap.remove(14);
+        cmap.remove(11);
+        cmap.remove(9);
+        cmap.remove(1);
+
+        for (int i = 0; i < 15; i++) {
+            System.out.printf("%s : %s \n", i, cmap.get(i));
+        }
+
+    }
+}
